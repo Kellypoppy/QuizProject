c4d089501d5e723ee8af5e99aee42b76f45bc243
Li Xuan <lixuanyap4@gmail.com>
2025-01-20 20:50:10 +0800

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+# ************************************************************************* 
+# Program: main.py 
+# Course: CSP1114 PROBLEM SOLVING AND PROGRAM DESIGN 
+# Lecture / Lab Section: TC1L / TL2L 
+# Trimester: 2430 
+# Names: YAP LI XUAN | MEMBER_NAME_2 
+# IDs: 242FC242JW | MEMBER_ID_2
+# Emails: YAP.LI.XUAN@student.mmu.edu.my | MEMBER_EMAIL_2 
+# ************************************************************************* 
