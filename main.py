6a33e86ff421c4a0c27dd560c19206e305d414f9
Eryne <erynechuah@gmail.com>
2025-01-30 21:04:28 +0800

    testing

--- main.py
+++ main.py
@@ -3,10 +3,20 @@
 # Course: CSP1114 PROBLEM SOLVING AND PROGRAM DESIGN 
 # Lecture / Lab Section: TC1L / TL2L 
 # Trimester: 2430 
-# Names: YAP LI XUAN | MEMBER_NAME_2 
-# IDs: 242FC242JW | MEMBER_ID_2
-# Emails: YAP.LI.XUAN@student.mmu.edu.my | MEMBER_EMAIL_2 
+# Names: YAP LI XUAN | ERYNE CHUAH EE WEN
+# IDs: 242FC242JW | 242FC2432F
+# Emails: YAP.LI.XUAN@student.mmu.edu.my | ERYNE.CHUAH.EE@student.mmu.edu.my
 # ************************************************************************* 
 
-def main():
-    pass
+with open('database_testing.txt', 'r') as text_file:
+    content = text_file.read()
+    print(content)
+
+def register():
+    userID = input("Enter user ID: ")
+    password = input("Enter password: ")
+    with open('database_testing.txt', 'a') as text_file:
+        add_user = text_file.write(userID + "\n")
+        add_pass = text_file.write(password +  "\n")
+    
+register()
